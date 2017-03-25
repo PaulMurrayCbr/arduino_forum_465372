0d479b5c0c6e3099f5270704c7ae9ca01c597512
Paul Murray <pmurray@bigpond.com>
2017-03-25 16:46:03 +1100

    Sat 25 Mar 2017 16:46:03 AEDT

--- arduino_forum_465372.ino
+++ arduino_forum_465372.ino
@@ -1,14 +1,17 @@
 
 
 enum State {
-IDLE, PAUSE_BEFORE_ON, ON_DELAY, OPEN_DELAY, WAIT_BEAM, PAUSE_BEFORE_CLOSE, OFF_DELAY
+  IDLE, PAUSE_BEFORE_ON, ON_DELAY, OPEN_DELAY, WAIT_BEAM, PAUSE_BEFORE_CLOSE, OFF_DELAY
 } state = IDLE;
 
-uint32_t start_time; // the time at which we entered the current state
-// we don't bother with start_time in IDLE state, but ON and OPEN are timed
+uint32_t start_time;
 
-const uint32_t ON_TIME = 500; // wait half a second for power up
-const uint32_t OPEN_TIME = 2000; // wait for 2 seconds after the beam is closed again
+const uint32_t PAUSE_BEFORE_ON_ms = 10L * 1000L;
+const uint32_t ON_DELAY_ms = 500L;
+const uint32_t OPEN_DELAY_ms = 500L;
+const uint32_t PAUSE_BEFORE_CLOSE_ms = 2L * 1000L;
+const uint32_t OFF_DELAY_ms = 250L;
+
 
 const byte onPin = 2;
 const byte openPin = 3;
@@ -29,17 +32,10 @@ void deactivateButton(byte pin) {
 void setup() {
   deactivateButton(onPin);
   deactivateButton(openPin);
-  // looking at the diagram in reply #8, it seems that
-  // the pin gets pulled low when the beam is broken
+  pinMode(beamPin, INPUT_PULLUP);
 
-  // if you are adding the recommended 5.1k resistor externally, then you don't
-  // need INPUT_PULLUP - because that 5.1k resistor is a pullup resistor.
-  // but, if you are just connecting the output pin directly, then you do need it
- 
-  pinMode(beamPin, INPUT_PULLUP); // assuming the 5.1k resistor is not in the circuit
-  // pinMode(beamPin, INPUT); // if that5.1k resistor is present
- 
   state = IDLE;
+  start_time = millis();
 }
 
 boolean beamIsBroken() {
@@ -48,56 +44,93 @@ boolean beamIsBroken() {
   return digitalRead(beamPin) == LOW;
 }
 
+
+/**
+
+  --wait for beam to break (IDLE)
+  --beam breaks
+  --wait 10 seconds  (PAUSE_BEFORE_ON)
+  --send signal to ON
+  --wait half a second  (ON_DELAY)
+  --send signal to OPEN
+  --wait half a second  (OPEN_DELAY)
+  --release/turn off/float both signals
+  --wait for beam to reconnect  (WAIT_BEAM)
+  --wait another 2 seconds (PAUSE_BEFORE_CLOSE)
+  --send ground signal to ON(which deactivates function)
+  --wait 1/4 of a second (OFF_DELAY)
+  --turn off ON
+  --wait for beam to break again  (IDLE)
+
+*/
+
 void loop() {
   switch (state) {
     case IDLE:
       if (beamIsBroken()) {
+        state = PAUSE_BEFORE_ON;
         start_time = millis();
-        activateButton(onPin);
-        state = ON;
       }
       break;
 
     case PAUSE_BEFORE_ON:
+      if (millis() - start_time >= PAUSE_BEFORE_ON_ms) {
+        activateButton(onPin);
+
+        state = ON_DELAY;
+        start_time = millis();
+      }
       break;
 
 
     case ON_DELAY:
-      if (millis() - start_time >= ON_TIME) {
-        start_time = millis();
-        // don't need to activate button A - it's already activated.
+      if (millis() - start_time >= ON_DELAY_ms) {
         activateButton(openPin);
-        state = OPEN;
+
+        state = OPEN_DELAY;
+        start_time = millis();
       }
       break;
 
     case OPEN_DELAY:
-      if(beamIsBroken()) {
-        // if the beam is broken, we reset the 2-second timeout to "now". This means that
-        // the dor should remain open until two seconds after the cat has come through the door.
-        start_time = millis();
-      }
-      else
-      if (millis() - start_time >= OPEN_TIME) {
-        // the beam has been unbroken for two seconds. Close the door.
+      if (millis() - start_time >= OPEN_DELAY_ms) {
         deactivateButton(openPin);
         deactivateButton(onPin);
-        state = IDLE;
-      }
-      else {
-        ; // do nothing and leave the door open
+
+        state = WAIT_BEAM;
+        start_time = millis();
       }
       break;
 
-      case WAIT_BEAM:
+    case WAIT_BEAM:
+      if (!beamIsBroken()) {
+        state = PAUSE_BEFORE_CLOSE;
+        start_time = millis();
+      }
       break;
-   
-   
+
+
     case PAUSE_BEFORE_CLOSE:
+      if (beamIsBroken()) {
+        state = WAIT_BEAM;
+        start_time = millis();
+      }
+      else  if (millis() - start_time >= PAUSE_BEFORE_CLOSE_ms) {
+        activateButton(onPin);
+
+        state = OFF_DELAY;
+        start_time = millis();
+      }
       break;
-   
-   
+
+
     case OFF_DELAY:
+      if (millis() - start_time >= OFF_DELAY_ms) {
+        deactivateButton(onPin);
+
+        state = IDLE;
+        start_time = millis();
+      }
       break;
   }
 }
